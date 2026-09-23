f7c745c5a248ad842a491a6faf7e5a6e56455fc0
Rohit-290 <rohit290kumar@gmail.com>
2026-09-23 17:43:04 +0530

    Updated on 23rd September 2026

--- src/components/News.js
+++ src/components/News.js
@@ -7,15 +7,43 @@ export class News extends Component {
       super();
       this.state = {
         results: [],
-        loading: false
+        loading: false,
+        page: ""
       }
   }
 
   async componentDidMount(){
-    let url = "https://newsdata.io/api/1/latest?apikey=pub_53d480ddddcf4af1a96729837e76a8af";
+    let url = "https://newsdata.io/api/1/latest?apikey=pub_8c68a2f176db4447a77fa9384ba418a6&country=in,gb,us,sa,au&language=en&image=1&video=0";
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({results: parsedData.results});
+    console.log(parsedData);
+    this.setState({
+      page: parsedData.nextPage ,
+      results: parsedData.results});
+  }
+
+  nextPage = async ()=>{
+    let url = `https://newsdata.io/api/1/latest?apikey=pub_8c68a2f176db4447a77fa9384ba418a6&country=in,gb,us,sa,au&language=en&image=1&video=0&page=${this.state.page}`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+        page: parsedData.nextPage,
+        results: parsedData.results
+       }
+       )
+  }
+
+   prevPage = async ()=>{
+    let url = `https://newsdata.io/api/1/latest?apikey=pub_53d480ddddcf4af1a96729837e76a8af&country=in,gb,us,sa,au&language=en&image=1&video=0&page=${this.state.page}`
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+        page: parsedData.nextPage ,
+        results: parsedData.results
+       }
+       )
   }
   
   render() {
@@ -28,6 +56,10 @@ export class News extends Component {
          <NewsItem title={ele.title} description={ele.description} imageUrl={ele.image_url} newsUrl={ele.link}/>
         </div>
         })}
+      </div>
+      <div className="container d-flex justify-content-between">
+        <button type="button" class="btn btn-dark" onClick={this.prevPage}>Previous</button>
+        <button type="button" class="btn btn-dark"onClick={this.nextPage}>Next</button>
       </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,11 +6,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3" id="item">
         <div className="card" style={{"width": "18rem"}}>
-        <img src={imageUrl?imageUrl:"news-placeholder.jpg"} class="card-img-top" alt="..."/>
+        <img src={imageUrl?imageUrl:"/news-placeholder.jpg"} className="card-img-top" alt="News"/>
         <div className="card-body">
         <h5 className="card-title">{title}...</h5>
         <p className="card-text">{description}...</p>
-        <button><a href={newsUrl} target="_blank" class="btn btn-primary">Read More</a></button>
+        <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-dark">Read More</a>
         </div>
       </div>
       </div>
